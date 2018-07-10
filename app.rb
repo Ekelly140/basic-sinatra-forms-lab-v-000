de354ebc99fd7a482b05f56a8c64de8348dc64cc
ERIC KELLY <eakelly220@gmail.com>
2018-07-10 19:31:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,6 +4,13 @@ class App < Sinatra::Base
 
   get '/newteam' do
     erb :newteam
+    @team = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @sf = params["sf"]
+    @pf = params["pf"]
+    @c = params["c"]
   end
 
 
